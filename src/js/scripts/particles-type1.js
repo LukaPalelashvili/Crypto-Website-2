d2a75cde1bdc641941fd47f6d06a786ab8187420
LukaPalelashvili <lukapalelashvili@gmail.com>
2021-10-27 02:30:56 +0400

    updates

--- src/js/scripts/particles-type1.js
+++ src/js/scripts/particles-type1.js
@@ -84,11 +84,11 @@ particlesJS('particles-js',
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": false,
+          "enable": true,
           "mode": "repulse"
         },
         "onclick": {
-          "enable": false,
+          "enable": true,
           "mode": "push"
         },
         "resize": true
